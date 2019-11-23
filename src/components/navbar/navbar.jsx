eea1c9f84e4e0b6296d0f6791142c409b609fec6
Giorgio Grassini <giorgiograssini@gmail.com>
2019-11-24 00:19:39 +0100

    Typo

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import './navbar.css';
+import React from "react";
+import "./navbar.css";
 
 const navbar = () => (
   <ul>
     <li>
-      <a href="https://github.com/Girgetto">GitHub</a>
-      <a href="https://twitter.com/__Giorgio_">Twitter</a>
-      <a href="https://www.girgetto-io.netify.com">Girgetto.io</a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/Girgetto"
+      >
+        GitHub
+      </a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://twitter.com/__Giorgio_"
+      >
+        Twitter
+      </a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://girgetto-io.netlify.com/"
+      >
+        Girgetto.io
+      </a>
     </li>
   </ul>
 );
