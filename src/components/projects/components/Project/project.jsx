ca62d6b87cb6165a2f4502e42af790c219bc76f5
Giorgio Grassini <giorgiograssini@gmail.com>
2019-11-25 19:49:09 +0100

    Shadow in project

--- src/components/projects/components/Project/project.jsx
+++ src/components/projects/components/Project/project.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./project.css";
 
+const navigate = site => window.open(`${site}`);
+
 const project = props => (
   <div className="slide-fwd-center">
-    <div className="column-wrapper">
-      <div className="col-6">
-        <a href={props.siteUrl} rel="noopener noreferrer" target="_blank">
+    <div className="linker" onClick={() => navigate(props.siteUrl)}>
+      <div className="column-wrapper">
+        <div className="col-6">
           <img
             className="frame"
             src={
@@ -15,32 +17,32 @@ const project = props => (
             }
             alt="img"
           />
-        </a>
-      </div>
-      <div className="col-6">
-        <div className="project-title">
-          <h2>{props.name}</h2>
-          {props.githubUrl && (
-            <a
-              className="github-button"
-              href={props.githubUrl}
-              data-size="large"
-              data-show-count="true"
-              aria-label="Star Girgetto/IronSpace on GitHub"
-            >
-              Star
-            </a>
-          )}
         </div>
-        <div
-          style={{
-            textAlign: "center",
-            display: "flex",
-            justifyContent: "center",
-            alignContent: "center"
-          }}
-        >
-          <p>{props.description}</p>
+        <div className="col-6">
+          <div className="project-title">
+            <h2>{props.name}</h2>
+            {props.githubUrl && (
+              <a
+                className="github-button"
+                href={props.githubUrl}
+                data-size="large"
+                data-show-count="true"
+                aria-label="Star Girgetto/IronSpace on GitHub"
+              >
+                Star
+              </a>
+            )}
+          </div>
+          <div
+            style={{
+              textAlign: "center",
+              display: "flex",
+              justifyContent: "center",
+              alignContent: "center"
+            }}
+          >
+            <p>{props.description}</p>
+          </div>
         </div>
       </div>
     </div>
