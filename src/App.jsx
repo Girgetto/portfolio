d600ab47a58e12d9475dcc57c14ac815bca646e9
Giorgio Grassini <giorgiograssini@gmail.com>
2020-10-09 11:57:15 +0200

    Fixed path

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
 import React from 'react';
-import Navbar from './components/Navbar/navbar';
-import About from './components/About/about';
-import Projects from './components/Projects/projects';
-import Footer from './components/Footer/footer';
-import Header from './components/Header/header';
+import Navbar from './components/navbar/navbar';
+import Header from './components/header/header';
+import About from './components/about/about';
+import Projects from './components/projects/projects';
+import Footer from './components/footer/footer';
 
 console.log('(̲̅G̲̅)(̲̅i̲̅)(̲̅o̲̅)(̲̅r̲̅)(̲̅g̲̅)(̲̅i̲̅)(̲̅o̲̅) (̲̅G̲̅)(̲̅r̲̅)(̲̅a̲̅)(̲̅s̲̅)(̲̅s̲̅)(̲̅i̲̅)(̲̅n̲̅)(̲̅i̲̅)))');
 
