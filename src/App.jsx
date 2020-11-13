28be713b7321309b87850ea0f72f84b65679814d
Giorgio Grassini <giorgiograssini@gmail.com>
2020-11-13 17:09:22 +0100

    fix(): unused var

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import Navbar from './components/navbar/navbar';
 import Header from './components/header/header';
 import About from './components/about/about';
-import Projects from './components/projects/projects';
 import Footer from './components/footer/footer';
 
 console.log('(̲̅G̲̅)(̲̅i̲̅)(̲̅o̲̅)(̲̅r̲̅)(̲̅g̲̅)(̲̅i̲̅)(̲̅o̲̅) (̲̅G̲̅)(̲̅r̲̅)(̲̅a̲̅)(̲̅s̲̅)(̲̅s̲̅)(̲̅i̲̅)(̲̅n̲̅)(̲̅i̲̅)))');
