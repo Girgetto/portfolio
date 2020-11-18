a636c132271a2d07ebf7ffa59d29bbc9c83dcb4f
Giorgio Grassini <giorgiograssini@gmail.com>
2020-11-18 11:37:59 +0100

    feat(): refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import './App.css';
-import React from 'react';
-import Navbar from './components/navbar/navbar';
-import Header from './components/header/header';
-import About from './components/about/about';
-import Footer from './components/footer/footer';
+import "./App.css";
+import React from "react";
 
-console.log('(̲̅G̲̅)(̲̅i̲̅)(̲̅o̲̅)(̲̅r̲̅)(̲̅g̲̅)(̲̅i̲̅)(̲̅o̲̅) (̲̅G̲̅)(̲̅r̲̅)(̲̅a̲̅)(̲̅s̲̅)(̲̅s̲̅)(̲̅i̲̅)(̲̅n̲̅)(̲̅i̲̅)))');
+import { Navbar, Header, About, Footer } from "./components";
+
+console.log("(̲̅G̲̅)(̲̅i̲̅)(̲̅o̲̅)(̲̅r̲̅)(̲̅g̲̅)(̲̅i̲̅)(̲̅o̲̅) (̲̅G̲̅)(̲̅r̲̅)(̲̅a̲̅)(̲̅s̲̅)(̲̅s̲̅)(̲̅i̲̅)(̲̅n̲̅)(̲̅i̲̅)))");
 
 const App = () => (
-	<div className="App">
-		<Navbar />
-		<Header />
-		<About />
-		<Footer />
-	</div>
+  <div className="App">
+    <Navbar />
+    <Header />
+    <About />
+    <Footer />
+  </div>
 );
 
 export default App;
