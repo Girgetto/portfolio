bd45c357ae60bbc126bc1358b42f7725c740adc0
Giorgio Grassini <giorgiograssini@gmail.com>
2020-11-18 11:42:57 +0100

    feat(): removed sass

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
-import Navbar from "./navbar/navbar";
-import Header from "./header/header";
-import About from "./about/about";
-import Footer from "./footer/footer";
+import Navbar from "./Navbar/navbar";
+import Header from "./Header/header";
+import About from "./About/about";
+import Footer from "./Footer/footer";
 
 export { Navbar, Header, About, Footer };
