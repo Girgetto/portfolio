e62db3aa31d347033e5fc1b1aec03f8b29ddd865
Giorgio Grassini <giorgiograssini@gmail.com>
2020-11-26 18:08:25 +0100

    github profile image

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import logo from '../../assets/img/profile.jpg';
+import React from "react";
+
+const gitHubImg = "https://avatars3.githubusercontent.com/u/33903092?s=460&u=6184ecd306816175a754a42d871329f02d457272&v=4"
 
 const header = () => (
   <header className="App-header">
-    <img src={logo} className="profile-logo" alt="logo" />
+    <img
+      src={gitHubImg}
+      className="profile-logo"
+      alt="logo"
+    />
     <h1 className="App-title">Giorgio Grassini</h1>
     <h2 className="Title-description container header-title">
       Fullstack Developer
