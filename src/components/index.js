7ec21bff1966bef577f61f50ccc4eb5a4b04475e
Giorgio Grassini <giorgiograssini@gmail.com>
2020-11-27 12:46:48 +0100

    lighthouse (#12)
    * improoves
    
    * widht and height

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Navbar from "./navbar/navbar";
 import Header from "./header/header";
 import About from "./about/about";
 import Footer from "./footer/footer";
+import renderLoader from "./renderLoader";
 
-export { Navbar, Header, About, Footer };
+export { Navbar, Header, About, Footer, renderLoader };
---
+++ src/components/renderLoader/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const renderLoader = () => (
+  <p
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+    }}
+  >
+    Loading
+  </p>
+);
+
+export default renderLoader;
