5cd493f92cfe1a1e72a9da71b37c85d999de012c
Giorgio Grassini <giorgiograssini@gmail.com>
2021-05-14 17:22:26 +0200

    description update

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -8,20 +8,18 @@ const About = () => (
     <div className="left">
       <h1 className="about-title">About me</h1>
       <p className="description">
-        I'm a big fan of the world of programming and technology. I put all of
-        myself into every project I do, trying to bring my ideas to reality. I
-        feel comfortable working in a group trying to inspire all my teammates.
-        I began to study the basics of programming from high school, I continued
-        my studies at the University of Florence. Moved to Madrid in 2017 to
-        increase my knowledge in programming at{" "}
-        <a target="blank" href={config.ironHackURL}>
-          IronHack
-        </a>{" "}
-        where I've worked as Teacher Assistant and from April 2019 part of the{" "}
+        I'm a Full stack developer based in Madrid📍, born and raised in italy
+        🇮🇹
+        <br />
+        Currently working at{" "}
         <a target="blank" href={config.guidesmithsURL}>
-          Guidesmiths
+          DSCL Guidesmiths
         </a>{" "}
-        team.
+        💻 full time, giving classes at{" "}
+        <a target="blank" href={"https://www.thebridge.tech/"}>
+          The Bridge
+        </a>{" "}
+        part time 📚
       </p>
     </div>
     <CodeThinking className="right" />
