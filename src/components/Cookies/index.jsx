a684e40082e94e087b0e53180d200c636b1fe2b3
Giorgio Grassini <giorgiograssini@gmail.com>
2021-06-18 17:48:52 +0200

    :zap: feat: Privacy Policy

--- src/components/Cookies/index.jsx
+++ src/components/Cookies/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import CookieConsent from "react-cookie-consent";
+import { Link } from "gatsby";
 
 export default function index() {
   return (
     <CookieConsent>
-      This website uses cookies to enhance the user experience.
+      This website uses cookies to enhance the user experience.{" "}
+      <Link style={{ color: "white" }} to="/privacy-policy">
+        Learn more
+      </Link>
     </CookieConsent>
   );
 }
